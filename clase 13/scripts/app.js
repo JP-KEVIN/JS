42b9017314e04bdc9db7795d1332ca5bd111bf68
yinismith <kevinjp0117@gmail.com>
2022-08-11 10:20:16 -0500

    java git commit -m ava git commit -m java

---
+++ clase 13/scripts/app.js
@@ -0,0 +1,12 @@
+const box = document.querySelector('.box')
+
+for (let i = 1; i <= 100; i++) {
+  const element = document.createElement('div')
+  element.innerHTML = i
+  element.className = 'element'
+  if(i      
+    
+    )
+  box.appendChild(element)
++
+}
---
+++ clase 2/Scripts/index.js
@@ -0,0 +1,17 @@
+// console.log(age);
+
+// console.log(nombre)
+
+// let nombre="carlos"
+
+{
+let age=5,
+firstname="santiago",
+lastname= "padierna suarez",
+isstudent=false,
+gender="male",
+city,
+country=null;
+
+console.log(typeof gender);    
+}
